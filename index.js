804ef07c367f1b0a2f0cfd266aa4cafd50c14a90
mostafizur044 <mostafizur.rahman@selise.ch>
2020-05-25 16:41:29 +0600

    Add saving previous input info

--- index.js
+++ index.js
@@ -7,62 +7,114 @@ cmd = require('node-run-cmd'),
 {host, mongoPath, status} = require('./lib/config');
 
 
-let dbName = '', mongoFolderName = '4.2', dumpCommand = ''; 
+let dbName = '', dumpCommand = ''; 
 const originalPath = files.getCurrentDirectoryPath();
-let mongoCmdpath = '';
+let mongoCmdpath = '', data = {};
 
 
 const run = async () => {
-    clear();
-    console.log(chalk.yellow(figlet.textSync('Mongo Dumping', { horizontalLayout: 'full' })));
-    await takingData();
-    await mongoDumping();
+    // clear();
+    console.log(files.directoryExists(originalPath +'/assets/data.json'))
+    if(files.directoryExists(originalPath +'/assets/data.json')) {
+        const d = files.getJSONFileData(originalPath +'/assets/data.json');
+        data = JSON.parse(d);
+        console.log(JSON.parse(d));
+    } else {
+        files.createDir(originalPath +'/assets');
+        files.writeJSONData(originalPath +'/assets/data.json', data);
+    }
+    
+    console.log(chalk.yellow(figlet.textSync('Mongo Export', { horizontalLayout: 'full' })));
+    const dirName = files.getParentDirectory().toString();
+    data = {...data, dirName};
+    await takingInputData();
+    await mongoDumpingOrExporting();
 };
 
-async function takingData() {
+async function takingInputData() {
     console.log('Current directory: ' + originalPath);
     console.log('\n**************** Input section ****************\n ');
     await takingDBname();
     await takingMongoVersion();
 
-    console.log('\nYour DB name: ' + dbName);
-    console.log('Your Mongo folder: ' + mongoFolderName);
-
     return new Promise(resolve => resolve('resolved'));
 }
 
 async function takingDBname() {
+    if(data.dbName) {
+        console.log('\nYour DB name: ' + data.dbName);
+        const {check} = await inquirer.askForUseExistingOne();
+        if(check.toString().toLowerCase() === 'y') {
+            formatDbName(data.dbName);
+        } else await manpulateDBName();
+    } else await manpulateDBName();
+}
+
+async function manpulateDBName() {
     const {name, check} = await inquirer.askDBname();
     dbName = name.toString().trim();
-    console.log(check);
     if(check.toString().toLowerCase() === 'y') {
-        const dirName = files.getParentDirectory().toString();
-        dumpCommand = '.\\mongodump.exe --host ' + host +' --db '+ dbName +' --out '+ dirName +'\\all-seed\\bson\\';
-        
+        formatDbName(dbName);
     } else {
-        await takingDBname();
-    }
-    
+        await manpulateDBName();
+    } 
+}
+
+function formatDbName(dbName) {
+    dumpCommand = '.\\mongodump.exe --host ' + host +' --db '+ dbName +' --out '+ data.dirName +'\\all-seed\\bson\\';
+    data = {...data, dbName};
+    files.writeJSONData(originalPath +'/assets/data.json', data);
+    console.log('\nYour DB name: ' + dbName);
 }
 
 async function takingMongoVersion() {
+    if(data.fname) {
+        console.log('Your Mongo folder: ' + data.fname);
+        const {check} = await inquirer.askForUseExistingOne();
+        if(check.toString().toLowerCase() === 'y') {
+            manpulateFName(data.fname);
+        } else await formatFName();
+    } else await formatFName();
+    
+}
+
+async function formatFName() {
     let {fname} = await inquirer.askMongoversion();
     if(fname) {
         // console.log(files.directoryExists(mongoPath + fname));
         fname = fname.toString().trim();
         if(files.directoryExists(mongoPath + fname)) {
-            mongoFolderName = fname;
-            mongoCmdpath = mongoPath  + mongoFolderName + '\\bin';
+            manpulateFName(fname);
         } else {
             console.log('Sorry wrong folder name');
-            await takingMongoVersion();
+            await formatFName();
         }
     }
 }
 
+function manpulateFName(fname) {
+    mongoCmdpath = mongoPath  + fname + '\\bin';
+    data = {...data, fname};
+    files.writeJSONData(originalPath +'/assets/data.json', data);
+    console.log('Your Mongo folder: ' + fname);
+}
 
-function mongoDumping () {
-    console.log('\n**************** Dumping section ****************\n ');
+
+async function mongoDumpingOrExporting () {
+    console.log('\n**************** Exporting section ****************\n ');
+
+    if(data.filenames && data.filenames.length > 0) {
+        console.log('Your Entity Names: ');
+        console.log(data.filenames);
+        const {check} = await inquirer.askForUseExistingOne();
+        if(check.toString().toLowerCase() === 'y') {
+            status.start();
+            exportMongocollection();
+        } else callMongoDump();
+    } else callMongoDump();
+}
+
+function callMongoDump() {
     files.changeCurrentDirectoryPath(mongoCmdpath);
     console.log(dumpCommand, '\nNew directory Path: ' + files.getCurrentDirectoryPath());
     status.start();
@@ -80,29 +132,25 @@ function onError(err) {
     process.exit();
 }
 
-async function onDumpingDone () {
+function onDumpingDone () {
     files.changeCurrentDirectoryPath(originalPath);
-    status.stop();
     console.log('\nOriginal directory: ' + files.getCurrentDirectoryPath());
-    console.log(files.directoryExists('../all-seed/bson/' + dbName));
-    status.stop();
-    let {check} = await inquirer.askForExport();
-    if(check.toString().toLowerCase() === 'y') {
-        exportMongocollection();
-    } else {
-        console.log(chalk.yellow(figlet.textSync('Done !!!')));
-        process.exit();
-    }
+    console.log(files.directoryExists('../all-seed/bson/' + data.dbName));
+
+    manpulateExport();
+    exportMongocollection();
+   
 };
 
+function manpulateExport() {
+    const filePath = data.dirName + '\\all-seed\\bson\\' + data.dbName;
+    data['filenames'] = files.getFileList(filePath).map( m => m.split('.')[0]).filter((item, i, arr) => arr.indexOf(item) === i);
+    files.writeJSONData(originalPath +'/assets/data.json', data);
+    files.removeDir(data.dirName + '\\all-seed\\bson');
+}
+
 function exportMongocollection() {
-    console.log('\n**************** Exporting section ****************\n ');
-    const dirName = files.getParentDirectory().toString();
-    const filePath = dirName + '\\all-seed\\bson\\' + dbName;
-    const filenames = files.getFileList(filePath).map( m => m.split('.')[0]).filter((item, i, arr) => arr.indexOf(item) === i);
-    console.log(filenames);
-    const exportCommands = filenames.map( m => '.\\mongoexport.exe --host ' + host +' --db '+ dbName +' --collection ' + m + ' --out '+ dirName +'\\all-seed\\json\\' + dbName +'\\'+ m +'.json');
-    status.start();
+    const exportCommands = data.filenames.map( m => '.\\mongoexport.exe --host ' + host +' --db '+ data.dbName +' --collection ' + m + ' --out '+ data.dirName +'\\all-seed\\json\\' + data.dbName +'\\'+ m +'.json');
     files.changeCurrentDirectoryPath(mongoCmdpath);
     console.log(exportCommands, '\nNew directory Path: ' + files.getCurrentDirectoryPath());
     
